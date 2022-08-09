9e18ce1887d6341e6526d213635bc8df834e6f51
gokulgk-9402 <gokulgk9402@gmail.com>
2022-08-09 19:16:42 +0530

    Initial commit for HashTable from scratch

---
+++ HashTables.py
@@ -0,0 +1,15 @@
+MAX_HASH_TABLE_SIZE = 4096 # cREATE A LIST OF A FIXED SIZE
+data_list = [None] * 4096
+
+class HashTable:
+    def insert(self, key, value):
+        pass
+
+    def find(self, key):
+        pass
+
+    def update(self, key, value):
+        pass
+
+    def print(self):
+        pass
